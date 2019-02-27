a42f193f08122d307b9967322d2eda991149f41f
Radu Potop <radu@wooptoo.com>
2019-02-27 22:29:55 +0000

    Add knight __json__ method

--- knight.py
+++ knight.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass, field
 
 from item import Item
@@ -8,6 +9,16 @@ from pos import Pos
 class Knight:
     id: str # One of: R,G,B,Y
     pos: Pos
+    status: str = 'LIVE'
     equipped: Item = None
     base_attack: int = 1
     base_defence: int = 1
+
+    def __json__(self):
+        return [
+            '[{}, {}]'.format(self.pos.x, self.pos.y),
+            '"{}"'.format(self.status),
+            self.equipped.name if self.equipped else None,
+            self.base_attack,
+            self.base_defence
+        ]
