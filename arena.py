1599eaa2865481b42d911001b7dc8175ffe633d0
Radu Potop <radu@wooptoo.com>
2019-02-27 14:06:14 +0000

    Add helper functions to Arena

--- arena.py
+++ arena.py
@@ -4,9 +4,25 @@ from dataclasses import dataclass, field
 def empty_matrix():
     return [[], [], [], [], [], [], [], []]
 
+
 @dataclass
 class Arena:
     board: list = field(default_factory=empty_matrix)
 
-    def move_knight():
+    def move_knight(knight, direction):
         pass
+
+    def _direction_to_pos(direction, old_pos):
+        pass
+
+    def _is_empty_square(pos):
+        return len(pos.knights) + len(pos.items) == 0
+
+    def _is_square_with_item(pos):
+        return len(pos.items) > 0
+
+    def _is_square_with_knight(pos):
+        return len(pos.knights) > 0
+
+    def _is_square_with_water(pos):
+        return (pos.x < 0 or pos.x > 8) and (pos.y < 0 or pos.y > 8)
